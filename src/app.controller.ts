2e1a885c6e8bd3d1a2f7a9263121edb10c28e7e5
kimwanyoung <dhks2869@gmail.com>
2024-02-17 22:25:18 +0900

    feat : Post 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author: string
+ * title: string
+ * content: string
+ * likeCount: number
+ * commentCount: number
+ */
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'test author',
+      title: 'this is test post',
+      content: 'test post',
+      likeCount: 10,
+      commentCount: 5,
+    };
   }
 }
