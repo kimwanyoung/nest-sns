bb2fcfd5027fc480b5afbdd8bf7ffd1b028a6d5a
kimwanyoung <dhks2869@gmail.com>
2024-02-22 14:48:47 +0900

    feat : 토큰 이용하여 signing 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from '../users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
   /**
    * 우리가 만드려는 기능
    *
@@ -27,4 +31,24 @@ export class AuthService {
    *      3. 모두 통과되면 찾은 사용자 정보 반환
    *      4, loginWithEmail에서 반환된 데이터를 기반으로 토큰생성
    */
+
+  /**
+   * Payload에 들어갈 정보
+   *
+   * 1) email
+   * 2) sub -> id
+   * 3) type : 'access' | 'refresh'
+   */
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken,
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
 }
