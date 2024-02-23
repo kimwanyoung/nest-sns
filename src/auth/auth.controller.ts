41ac4d5c31e5f3c7d86d66a89138707fe4be2646
kimwanyoung <dhks2869@gmail.com>
2024-02-23 19:15:06 +0900

    feat : jwt이용해서 로그인, 회원가입 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('token/access')
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    /**
+     * {accessToken: {token}}
+     */
+    const newToken = this.authService.rotateToken(token, false);
+    return {
+      accessToken: newToken,
+    };
+  }
+
+  @Post('token/refresh')
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    /**
+     * {accessToken: {token}}
+     */
+    const newToken = this.authService.rotateToken(token, true);
+    return {
+      refreshToken: newToken,
+    };
+  }
+
+  @Post('login/email')
+  postLoginEmail(@Headers('authorization') rawToken: string) {
+    // email:password -> base64
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodeBasicToken(token);
+
+    return this.authService.loginWithEmail(credentials);
+  }
+
+  @Post('register/email')
+  postRegisterEmail(
+    @Body('nickname') nickname: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.registerWithEmail({
+      nickname,
+      email,
+      password,
+    });
+  }
 }
