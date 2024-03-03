87976cfed8895e3278e3b65322d2f594b9088428
kimwanyoung <dhks2869@gmail.com>
2024-03-03 12:32:20 +0900

    feat : auth2 제거

--- src/auth2/auth2.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { Auth2Service } from './auth2.service';
-
-@Controller('auth2')
-export class Auth2Controller {
-  constructor(private readonly auth2Service: Auth2Service) {}
-}
