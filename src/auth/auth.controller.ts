582f6f43c3b85aae60d4d63a20a30819e0399061
kimwanyoung <dhks2869@gmail.com>
2024-03-04 16:58:03 +0900

    feat : 비밀번호 검증 pipe 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Headers, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MaxLengthPipe, MinLengthPipe, PasswordPipe } from './pipe/password.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -40,8 +41,8 @@ export class AuthController {
   postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
+    @Body('password', new MaxLengthPipe(8), new MinLengthPipe(3)) password: string,
+    ) {
     return this.authService.registerWithEmail({
       nickname,
       email,
